084327b8b0f3a2cc3ac8662f4f6cc9c42d472441
shaik saidavali <shaiksaidavali223@gmail.com>
2025-09-27 17:10:40 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0e09997fe1acc7cb9"
+ZONE_ID="Z00521733RMRV4Y25CJSX"
+DOMAIN_NAME="daws86a.store"
 
 for instance in $@
 do 
@@ -11,10 +13,31 @@ INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro 
 if [ $instance != "frontend" ]; then
 
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    RECORD_NAME= "$instance.$DOMAIN_NAME"
 else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    RECORD_NAME= "$DOMAIN_NAME"
 
 fi
     echo "$instance = $IP"
+
+        aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 
 done
