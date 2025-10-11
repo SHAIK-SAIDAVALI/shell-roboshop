a74f4353367395d30331a44ab813dc11e68e57cb
shaik saidavali <shaiksaidavali223@gmail.com>
2025-10-11 23:00:16 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -38,4 +38,10 @@ VALIDATE $? "Enabiling MongoDB"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start Mongodb"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connection to Mongodb"
+
+systemctl restart mongod
+VALIDATE $? "restarted Mongodb"
+
 
