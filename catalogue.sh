4913ffc98e97d51db22327261a62fdedd9e1608a
shaik saidavali <shaiksaidavali223@gmail.com>
2025-10-12 00:40:03 +0530

    mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -91,6 +91,7 @@ VALIDATE $? "install mongodb client"
 
 
 INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
